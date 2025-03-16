d629f998abb69b00231cf1493f63efa548cf866b
VIAUD Nathan <nathan.viaud@inria.fr>
2025-03-16 10:43:33 +0100

    add education

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,32 +1,46 @@
 import { z, defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { Inspect } from "lucide";
 
 const projects = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./content/projects" }),
-    schema: z.object({
-        title: z.string(),
-        program: z.string(),
-        order: z.number(),
-        images: z.array(z.string()),
-        site: z.string(),
-    }),
+  loader: glob({ pattern: "**/*.md", base: "./content/projects" }),
+  schema: z.object({
+    title: z.string(),
+    program: z.string(),
+    order: z.number(),
+    images: z.array(z.string()),
+    site: z.string(),
+  }),
 });
 
 const annex = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./content/annex" }),
-    schema: z.object({
-        title: z.string(),
-        images: z.array(z.string()),
-        category: z.enum(["plastic", "photo", "digital"]),
-    })
-})
-
-const index = defineCollection({
-    loader: glob({ pattern: "index.md", base: "./content" }),
-    schema: z.object({
-        heading: z.string(),
-        description: z.string(),
-    }),
+  loader: glob({ pattern: "**/*.md", base: "./content/annex" }),
+  schema: z.object({
+    title: z.string(),
+    images: z.array(z.string()),
+    category: z.enum(["plastic", "photo", "digital"]),
+  }),
 });
 
-export const collections = { projects, index, annex };
+const index = defineCollection({
+  loader: glob({ pattern: "index.md", base: "./content" }),
+  schema: z.object({
+    heading: z.string(),
+    description: z.string(),
+  }),
+});
+
+const education = defineCollection({
+  loader: glob({ pattern: "education.yml", base: "./content" }),
+  schema: z.array(
+    z.object({
+      diploma: z.string(),
+      institution: z.string(),
+      start: z.string(),
+      end: z.string().optional(),
+      image: z.string(),
+    }),
+  ),
+});
+
+export const collections = { projects, index, annex, education };
